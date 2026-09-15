2ed77eab8ba09704f0e9f3e2742f41ed7e36748b
Xianw <xianwen122@gmail.com>
2026-09-15 00:21:35 -0400

    added diff function

---
+++ main.py
@@ -0,0 +1,14 @@
+def sum(num1, num2):
+    sum = num1 + num2
+    return sum
+
+ans = sum(4,5)
+
+def diff(num1, num2):
+    diff = num1 - num2
+    return diff
+
+ans2 = diff(4, 5)
+
+print("Sum is: ", ans)
+print("Diff is: ", ans2)
